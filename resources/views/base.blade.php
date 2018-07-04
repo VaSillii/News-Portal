e1c47778afcc1dcb460877ad72b897f9dfadd102
Vasilii <vk937937@gmail.com>
2018-07-05 01:49:48 +0300

    update laravel 5.6

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -1,8 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>{{$title or ''}}</title>
-	<link rel="stylesheet" type="text/css" href="base_style.css">
+	<title>{{$title or 'News Portal'}}</title>
 	@section('head')
 	@show
 </head>
--- resources/views/pages/content2.blade.php
+++
@@ -1,2 +0,0 @@
-<h1>{{$mainContent or ''}}</h1>
-
